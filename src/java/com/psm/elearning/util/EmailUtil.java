8c72663f497ab2440d6f93d6a43cd4485016c6e5
Fagacie <120310003+Fagacie@users.noreply.github.com>
2026-06-08 12:07:21 +0800

    feat: implement EmailUtil for sending text and HTML emails with configurable SMTP settings

--- src/java/com/psm/elearning/util/EmailUtil.java
+++ src/java/com/psm/elearning/util/EmailUtil.java
@@ -163,6 +163,13 @@ public class EmailUtil {
         props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.ssl.trust", host);
         
+        // Add SSL support for port 465
+        if ("465".equals(port)) {
+            props.put("mail.smtp.socketFactory.port", port);
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.ssl.enable", "true");
+        }
+        
         final String authUser = username;
         final String authPass = password;
         Session session = Session.getInstance(props, new Authenticator() {
